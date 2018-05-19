367ae58e79234f89f07fe2759d442a75695a4b39
li <lishuang@zjrealtech.com>
2018-05-19 17:36:01 +0800

    新增公共函数

---
+++ globalFunc/global.js
@@ -0,0 +1,68 @@
+(function (window, document) {
+  
+  // 函数扩展
+  
+  // 日期扩展 new Date('time').Format(xx)
+  Date.prototype.Format = function (fmt) { 
+    if (!fmt) {
+      fmt = 'yyyy-MM-dd hh:mm:ss'
+    }
+
+    var o = {
+      'M+': this.getMonth() + 1, // 月份
+      'd+': this.getDate(), // 日
+      'h+': this.getHours(), // 小时
+      'm+': this.getMinutes(), // 分
+      's+': this.getSeconds(), // 秒
+      'q+': Math.floor((this.getMonth() + 3) / 3), // 季度
+      'S': this.getMilliseconds() // 毫秒
+    }
+
+    if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (this.getFullYear() + ''.substr(4 - RegExp.$1.length)))
+    }
+
+    for (var k in o) {
+      if (new RegExp('(' + k + ')').test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(('' + o[k]).length)))
+      }
+    }
+
+    return fmt
+  }
+
+  let oLs = {
+      /*
+      * 占位符替换
+      * @param {String} sContent 含占位符的字符串
+      * @param {Object} oData要替换占位符的点位父数据，根据对象的键名与点位符一一对应
+      * @param {String} sUnValue 当值为空、null、NaN无效值时，使用该值来替换，如果该值也无效，则使用原来的值
+      * @return {String} 返回替换后的字符串
+      */
+    // window.Ls.utils.substitute()
+    substitute: function(sContent, oData, sUnValue) {
+      if (!oData) {
+        return sContent
+      }
+
+      let sValue = ""
+      for (let p in oData) {
+        sValue = oData[p]
+
+        if (!sValue && sValue !== undefined) {
+          sValue = sUnValue
+        }
+
+        sContent = sContent.replace(new RegExp('\\{' + p + '\\}', 'g'), sValue)
+        // sContent = sContent.replace(//g, sValue) 变量处理？
+      }
+
+      return sContent
+    }
+
+  }
+
+  window.Ls = {}
+  window.Ls.utils = oLs
+
+})(window, document)
